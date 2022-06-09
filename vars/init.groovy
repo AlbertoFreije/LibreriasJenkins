d52f328c2777b5f7f3b57821e13f5b856e5b53ac
AlbertoFreije <freicartobe@gmail.com>
2022-06-09 12:21:59 +0200

    Update init.groovy

--- vars/init.groovy
+++ vars/init.groovy
@@ -4,4 +4,8 @@ def call() {
     def inputFile = input message: 'Upload file', parameters: [file(name: nombreXML)]
         writeFile(file: nombreXML, text: inputFile.readToString())
         echo fileExists(nombreXML).toString()
+        def file = readFile "owasp-quick-scan-report.xml"
+        def xml = new XmlParser().parseText(file)
+        //echo "${xml}"
+        println(xml.value.site.attributes.name)
 }
